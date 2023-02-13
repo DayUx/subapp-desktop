29263b326c8d769af0131d043a6a82a0a7042996
Paul Coignac <paulcoignac@gmail.com>
2023-02-13 19:47:54 +0100

    L'application react est maintenant accessible sur le port 8080 à l'exécution du jar

--- src/main/java/fr/subapp/subappdesktop/SubappDesktopApplication.java
+++ src/main/java/fr/subapp/subappdesktop/SubappDesktopApplication.java
@@ -2,10 +2,10 @@ package fr.subapp.subappdesktop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SubappDesktopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SubappDesktopApplication.class, args);
     }
