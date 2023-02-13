b607cc585054d86de2384236e0a855b190730c15
Paul Coignac <paulcoignac@gmail.com>
2023-02-13 21:53:11 +0100

    Mise en place de ant design

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useState } from 'react'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import {Button, Layout, Menu, theme} from 'antd';
+
+import logo_l_white from './assets/logo_l_white.png'
+import logo_s_white from './assets/logo_s_white.png'
+
+const { Header, Sider, Content } = Layout;
 import './App.css'
+import Icon from "antd/es/icon";
 
 function App() {
   const [count, setCount] = useState(0)
-
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <Layout style={{height:"100vh"}}>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo " >
+              <img src={collapsed ? logo_s_white:logo_l_white}/>
+          </div>
+          <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+          />
+        </Sider>
+        <Layout className="site-layout" >
+          <Header style={{ padding: '0 16px', background: colorBgContainer }}>
+              <Button onClick={
+                  () => setCollapsed(!collapsed)
+              } icon={collapsed ?<MenuUnfoldOutlined></MenuUnfoldOutlined>  : <MenuFoldOutlined></MenuFoldOutlined>}  type={'primary'}></Button>
+
+          </Header>
+          <Content
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                background: colorBgContainer,
+              }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
   )
 }
 
